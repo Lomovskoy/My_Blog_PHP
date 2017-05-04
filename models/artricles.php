5067414cc6fa49227da3032ca2e279d5a6f58f1d
Lomovskoy <lomovskoy.kirill@yandex.ru>
2017-05-04 16:33:44 +0300

    Спроектированна MVС структура

---
+++ models/artricles.php
@@ -0,0 +1,22 @@
+<?php
+    function artricles_all(){
+        
+    }
+    
+    function artricles_get($id){
+        
+    }
+    
+    function artricles_new($title, $date, $content){
+        
+    }
+    
+    function artricles_edit($id, $title, $date, $content){
+        
+    }
+    
+    function artricles_delete($id){
+        
+    }
+?>
+
