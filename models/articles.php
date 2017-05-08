2b93e5f4c47d77959dd15daab5fd5e9bda648966
Lomovskoy <lomovskoy.kirill@yandex.ru>
2017-05-08 15:05:08 +0300

    Движок блока доделан полностью, добавленна возможность загружать, удалять и изменять фотографии прикреплённыен к постам

--- models/articles.php
+++ models/articles.php
@@ -33,7 +33,7 @@
 
     function articles_new($link, $title, $date, $content, $image){
         //--------------------------------------
-        include("upload.php");//Удаление самого файла
+        include("upload.php");//Загрузка самого файла
         //--------------------------------------
         $image = $_FILES['image']['name'];
         
@@ -64,15 +64,28 @@
     }
 
     function articles_edit($link, $id, $title, $date, $content, $image){
+        // Проверка
+        if ($title == '')
+            return false;
+        
+        // удаление файла картинки
+        //-------------------------
+        $query_dell = sprintf("SELECT image FROM articles WHERE id=%d", $id);
+        $result_dell = mysqli_query($link, $query_dell);
+        $n_dell = mysqli_num_rows($result_dell);
+        $row = mysqli_fetch_assoc($result_dell);
+        
+        include("delete.php");
+        delete_image($row);
+        //-----------------------
+        
         // Подготовка
         $title = trim($title);
         $content = trim($content);
         $date = trim($date);
         $id = (int)$id;
-        $image = trim($image);
-        // Проверка
-        if ($title == '')
-            return false;
+        $imagebd = $_FILES['image']['name'];
+        $imagebd = trim($imagebd);
         
         // Запрос
         $template_update = "UPDATE articles SET title='%s', content='%s', "
@@ -82,10 +95,14 @@
                          mysqli_real_escape_string($link, $title),
                          mysqli_real_escape_string($link, $content),
                          mysqli_real_escape_string($link, $date),
-                         mysqli_real_escape_string($link, $image),
+                         mysqli_real_escape_string($link, $imagebd),
                          $id);
         
         $result = mysqli_query($link, $query);
+        
+        //--------------------------------------
+        include("upload.php");//Загрузка самого файла
+        //--------------------------------------
         
         if (!result)
             die(mysqli_error($link));
